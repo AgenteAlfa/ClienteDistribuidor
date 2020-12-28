07b8598017ff85ffaa4b00be3935bc3a7bde197f
andya <afranco582@gmail.com>
2020-12-27 21:15:36 -0500

    ProgressBar y JLabel Resultado

--- src/com/distribuido/Ventana/Ventana.java
+++ src/com/distribuido/Ventana/Ventana.java
@@ -10,6 +10,8 @@ public class Ventana extends JFrame{
 
     public Ventana(){
         setSize(ancho, alto);
+        int PY1=515;
+        int PY2=580;
 
         this.setContentPane(fondo);
         fondo.setLayout(null);
@@ -17,48 +19,74 @@ public class Ventana extends JFrame{
         //Label A
         JLabel A = new JLabel();
         A.setText("a:");
-        A.setBounds(50,540,55,55);
+        A.setBounds(50,PY1,55,55);
         A.setFont(new Font("Arial",Font.BOLD,50));
         add(A);
         //JTextFieldA
         JTextField JTextFieldA = new JTextField();
         JTextFieldA.setText("0");
-        JTextFieldA.setBounds(100,540,70,55);
+        JTextFieldA.setBounds(100,PY1,70,55);
         JTextFieldA.setFont(new Font("Arial",Font.BOLD,35));
         add(JTextFieldA);
 
         //Label B
         JLabel B = new JLabel();
         B.setText(", b:");
-        B.setBounds(180,540,80,55);
+        B.setBounds(180,PY1,80,55);
         B.setFont(new Font("Arial",Font.BOLD,50));
         add(B);
         //JTextFieldB
         JTextField JTextFieldB = new JTextField();
         JTextFieldB.setText("10");
-        JTextFieldB.setBounds(260,540,70,55);
+        JTextFieldB.setBounds(260,PY1,70,55);
         JTextFieldB.setFont(new Font("Arial",Font.BOLD,35));
         add(JTextFieldB);
 
         //Label FX
         JLabel FX = new JLabel();
         FX.setText(", f(x):");
-        FX.setBounds(330,540,140,55);
+        FX.setBounds(330,PY1,140,55);
         FX.setFont(new Font("Arial",Font.BOLD,50));
         add(FX);
-        //JTextFieldB
+        //JTextFieldFX
         JTextField JTextFieldFX = new JTextField();
         JTextFieldFX.setText("10");
-        JTextFieldFX.setBounds(460,540,250,55);
+        JTextFieldFX.setBounds(460,PY1,250,55);
         JTextFieldFX.setFont(new Font("Arial",Font.BOLD,35));
         add(JTextFieldFX);
+
+        //Label N
+        JLabel N = new JLabel();
+        N.setText(", n:");
+        N.setBounds(710,PY1,80,55);
+        N.setFont(new Font("Arial",Font.BOLD,50));
+        add(N);
+        //JTextFieldN
+        JTextField JTextFieldN = new JTextField();
+        JTextFieldN.setText("10");
+        JTextFieldN.setBounds(790,PY1,120,55);
+        JTextFieldN.setFont(new Font("Arial",Font.BOLD,35));
+        add(JTextFieldN);
 
         //Boton calcular
         JButton BCalcular = new JButton();
         BCalcular.setText("Calcular");
-        BCalcular.setBounds(360,600,150,55);
+        BCalcular.setBounds(400,PY2,150,45);
+        BCalcular.setFont(new Font("Arial",Font.BOLD,25));
         BCalcular.setMnemonic('c');//tecla para hacerlo funcionar ALT+c
         add(BCalcular);
+
+        //ProgressBar
+        JProgressBar BProgreso = new JProgressBar();
+        BProgreso.setBounds(300,640,350,5);
+        add(BProgreso);
+
+        //Label R
+        JLabel R = new JLabel();
+        R.setText("Resultado:");
+        R.setBounds(310,650,300,55);
+        R.setFont(new Font("Arial",Font.BOLD,25));
+        add(R);
 
         setVisible(true);
 
